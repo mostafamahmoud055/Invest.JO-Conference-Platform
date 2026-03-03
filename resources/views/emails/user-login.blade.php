0ed61d8332ea5be85f918613e5c6ee293c796f9a
Mostafa Mahmoud <mostafamahmoud055@gmail.com>
2026-03-03 12:07:03 +0200

    done

--- resources/views/emails/user-login.blade.php
+++ resources/views/emails/user-login.blade.php
@@ -1,44 +1,76 @@
-<tr><td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px"><span style="font-size:13.33px;white-space:pre-wrap">Dear {{ $user->Profile->first_name }},</span><span style="font-size:15px;white-space:pre-wrap"><br></span></td></tr>
-<tr><td style="font-size:0;height:16px" height="16">&nbsp;</td></tr>
+<tr>
+    <td align="center">
+        <img src="{{ asset('images/aafa5c6c25d478a367dd450f64b222a7.png') }}" width="600"
+            style="display:block;width:100%;height:auto;max-width:100%">
+    </td>
+</tr>
 
-<tr><td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px"><span style="font-size:13.33px;white-space:pre-wrap">We welcome you to the Jordan–EU 2026 Conference and we value your participation.</span><span style="font-size:15px;white-space:pre-wrap"><br></span></td></tr>
-<tr><td style="font-size:0;height:16px" height="16">&nbsp;</td></tr>
+<tr>
+    <td style="font-size:0;height:16px" height="16">&nbsp;</td>
+</tr>
 
-<tr><td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px"><span style="font-size:13.33px;white-space:pre-wrap">Kindly access the conference application or website to manage your participation, schedule meetings, and complete your travel and accommodation details.</span><span style="font-size:15px;white-space:pre-wrap"><br></span></td></tr>
-<tr><td style="font-size:0;height:16px" height="16">&nbsp;</td></tr>
+<tr>
+    <td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px">
+        <span style="font-size:13.33px;white-space:pre-wrap">
+            Dear {{ $user->Profile->first_name }},
+        </span><br>
+    </td>
+</tr>
 
-<tr><td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px">
-<span style="font-size:13.33px;white-space:pre-wrap">Access details:</span><br><br>
-<span style="font-size:13.33px;white-space:pre-wrap">• Conference website / registration portal: {{ $user->Profile?->website }}</span><br>
-<span style="font-size:13.33px;white-space:pre-wrap">• *for now until the app link is available, amend line in yellow to: "We will share a link to the event's application soon. Please ensure to download it to have access to the event details and latest new updates."</span><br>
-<span style="font-size:13.33px;white-space:pre-wrap">• Hotel booking link: [Insert Hotel Link]</span>
-<span style="font-size:15px;white-space:pre-wrap"><br></span>
-</td></tr>
+<tr>
+    <td style="font-size:0;height:16px" height="16">&nbsp;</td>
+</tr>
 
-<tr><td style="font-size:0;height:16px" height="16">&nbsp;</td></tr>
+<tr>
+    <td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px">
+        <span style="font-size:13.33px;white-space:pre-wrap">
+            We welcome you to the Jordan–EU 2026 Conference and we value your participation.
+        </span><br>
+    </td>
+</tr>
 
-<tr><td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px">
-<span style="font-size:13.33px;white-space:pre-wrap">There will be a gala dinner held on the 20th of April, 2026. If you would like to confirm your attendance, please let us know.</span>
-<span style="font-size:15px;white-space:pre-wrap"><br></span>
-</td></tr>
+<tr>
+    <td style="font-size:0;height:16px" height="16">&nbsp;</td>
+</tr>
 
-<tr><td style="font-size:0;height:16px" height="16">&nbsp;</td></tr>
+<tr>
+    <td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px">
+        <span style="font-size:13.33px;white-space:pre-wrap">
+            Kindly access the conference application or website to manage your participation, schedule meetings, and
+            complete your travel and accommodation details.
+        </span><br>
+    </td>
+</tr>
 
-<tr><td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px">
-<span style="font-size:13.33px;white-space:pre-wrap">Providing your flight and accommodation information will enable us to coordinate airport transfers and ensure smooth arrival and departure arrangements.</span>
-<span style="font-size:15px;white-space:pre-wrap"><br></span>
-</td></tr>
+<tr>
+    <td style="font-size:0;height:16px" height="16">&nbsp;</td>
+</tr>
 
-<tr><td style="font-size:0;height:16px" height="16">&nbsp;</td></tr>
+<tr>
+    <td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px">
+        <span style="font-size:13.33px;white-space:pre-wrap">Access details:</span><br><br>
+        <span style="font-size:13.33px;white-space:pre-wrap">• Conference website / registration portal:
+            {{ $user->Profile?->website }}</span><br>
+        <span style="font-size:13.33px;white-space:pre-wrap">• *for now until the app link is available, amend line in
+            yellow to: "We will share a link to the event's application soon. Please ensure to download it to have
+            access to the event details and latest new updates."</span><br>
+        <span style="font-size:13.33px;white-space:pre-wrap">• Hotel booking link: [Insert Hotel Link]</span>
+    </td>
+</tr>
 
-<tr><td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px">
-<span style="font-size:13.33px;white-space:pre-wrap">Should you require assistance with visa procedures prior to travel, please inform us and our team will provide the necessary support.</span>
-<span style="font-size:15px;white-space:pre-wrap"><br></span>
-</td></tr>
+<tr>
+    <td style="font-size:0;height:16px" height="16">&nbsp;</td>
+</tr>
 
-<tr><td style="font-size:0;height:16px" height="16">&nbsp;</td></tr>
-
-<tr><td dir="ltr" style="font-size:16px;text-align:left;padding:0px 20px">
-<span style="font-size:13.33px;white-space:pre-wrap">Kind regards,</span>
-<span style="font-size:15px;white-space:pre-wrap"><br></span>
-</td></tr>
+<tr>
+    <td align="center">
+        <img src="{{ asset('images/8feca20d141ddd3e3eb3c659da2e2a2c.png') }}" width="560"
+            style="display:block;width:100%;height:auto;max-width:100%">
+    </td>
+</tr>
+<tr>
+    <td align="center">
+        <img src="{{ asset('images/93636a3607700868a90eab7fe5487b12.png') }}" width="560"
+            style="display:block;width:100%;height:auto;max-width:100%">
+    </td>
+</tr>
